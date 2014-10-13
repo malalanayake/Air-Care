0fde43b9db60e09f2f6f5d56dce035a3fb74057e
malalanayake <dinuka.malalanayake@gmail.com>
2014-10-13 00:08:42 -0500

    Add Jave Docs

--- src/main/java/org/air/care/controller/ClientController.java
+++ src/main/java/org/air/care/controller/ClientController.java
@@ -4,10 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+/**
+ * Main Client Controller
+ * 
+ * @author malalanayake
+ *
+ */
 @Controller
 @RequestMapping("/client")
 public class ClientController {
 
+	/**
+	 * Handle client home request
+	 * 
+	 * @return
+	 */
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String clientHome() {
 		return "/client/home";
--- src/main/java/org/air/care/controller/SecurityController.java
+++ src/main/java/org/air/care/controller/SecurityController.java
@@ -5,20 +5,43 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+/**
+ * Main Security Controller
+ * 
+ * @author malalanayake
+ *
+ */
 @Controller
 public class SecurityController {
 
+	/**
+	 * Handle login page request
+	 * 
+	 * @return
+	 */
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login() {
 		return "/security/login";
 	}
 
+	/**
+	 * Handle login failed request
+	 * 
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(value = "/loginfailed", method = RequestMethod.GET)
 	public String loginerror(Model model) {
 		model.addAttribute("error", "true");
 		return "/security/login";
 	}
 
+	/**
+	 * Handle logout request
+	 * 
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(value = "/logout", method = RequestMethod.GET)
 	public String logout(Model model) {
 		return "/security/login";
